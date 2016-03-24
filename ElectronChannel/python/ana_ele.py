4c66ec96a172de8ef7ea4fcb0e01a98f9e31614b
fanxia <fanxia08@gmail.com>
2016-03-24 13:47:45 +0100

    add ele module

---
+++ ElectronChannel/python/ana_ele.py
@@ -0,0 +1,18 @@
+# include functions for ele selection
+import os
+import sys
+import ROOT
+
+
+
+def good_TightEle(pt,eta,eleid):
+    
+    if pt>30 and abs(eta)<2.5 and (eleid>>3&1)==1:
+
+        return true
+
+def good_LooseEle(pt,eta,eleid):
+    
+    if pt>10 and abs(eta)<2.5 and (eleid>>1&1)==1:
+
+        return true
