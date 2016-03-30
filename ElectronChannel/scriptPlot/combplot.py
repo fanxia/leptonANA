7448c1265fab21d44207a0d3fa5a69283cc1ef20
fanxia <fanxia08@gmail.com>
2016-03-30 22:16:36 +0200

    plot

--- ElectronChannel/scriptPlot/combplot.py
+++ ElectronChannel/scriptPlot/combplot.py
@@ -18,14 +18,33 @@ xsec_ttjets=831.76
 lumi_ttjets=51.44
 frac_ttjets=lumi_data/lumi_ttjets
 
-xsec_ttwjets=0.2043
-lumi_ttwjets=1238
-frac_ttwjets=lumi_data/lumi_ttwjets
+xsec_ttw=0.2043
+lumi_ttw=1238
+frac_ttw=lumi_data/lumi_ttw
+
+xsec_dyjets=6025.2
+lumi_dyjets=4.77
+frac_dyjets=lumi_data/lumi_dyjets
+
+xsec_wjets=61526.7
+lumi_wjets=0.393
+frac_wjets=lumi_data/lumi_wjets
+
+xsec_tt=831.76
+lumi_tt=22.16
+frac_tt=lumi_data/lumi_tt
+
+xsec_ttg=3.697
+lumi_ttg=1307.1
+frac_ttg=lumi_data/lumi_ttg
+
+
+
 
 
 # stack plot module
 
-def stack(data,dataname,bkg1,bkg1name,frac1,bkg2,bkg2name,frac2,plotname,branchname):
+def stack(plotname,branchname,data,dataname,bkg1,bkg1name,frac1,bkg2,bkg2name,frac2,bkg3,bkg3name,frac3,bkg4,bkg4name,frac4,bkg5,bkg5name,frac5):
 
     c=ROOT.TCanvas("c","Plots",800,800)
     gStyle.SetOptStat(0)
@@ -47,6 +66,23 @@ def stack(data,dataname,bkg1,bkg1name,frac1,bkg2,bkg2name,frac2,plotname,branchn
     histmc2.SetFillColor(kBlue-10)
     histmc2.Scale(frac2)
     hs.Add(histmc2)
+
+    bkg3.Draw(branchname+">>histmc3")
+    histmc3.SetFillColor(kOrange)
+    histmc3.Scale(frac3)
+    hs.Add(histmc3)
+
+    bkg4.Draw(branchname+">>histmc4")
+    histmc4.SetFillColor(kViolet-1)
+    histmc4.Scale(frac4)
+    hs.Add(histmc4)
+
+    bkg5.Draw(branchname+">>histmc5")
+    histmc5.SetFillColor(kGreen+1)
+    histmc5.Scale(frac5)
+    hs.Add(histmc5)
+
+
 
     hs.SetTitle(plotname+";;")
 
@@ -144,18 +180,21 @@ def stackpho(plotname,branchname,region,data,dataname,bkg1,bkg1name,frac1,bkg2,b
     c.cd()
 
 #---------------bkg stack-------
- if region==11: #sr1,only one photon
-    bkg1pho_ind=bkg1.pho_index
-    bkg2pho_ind=bkg2.pho_index
-    datapho_ind=data.pho_index
- elif region==21: #sr2,leading photon
-    bkg1pho_ind=bkg1.pho_index[0]
-    bkg2pho_ind=bkg2.pho_index[0]
-    datapho_ind=data.pho_index[0]
- elif region==22: #sr2,trailing photon
-    bkg1pho_ind=bkg1.pho_index[1]
-    bkg2pho_ind=bkg2.pho_index[1]
-    datapho_ind=data.pho_index[1]
+ #sr1,only one photon
+    if region==11:
+        bkg1pho_ind=bkg1.pho_index
+        bkg2pho_ind=bkg2.pho_index
+        datapho_ind=data.pho_index
+ #sr2,leading photon
+    elif region==21:
+        bkg1pho_ind=bkg1.pho_index[0]
+        bkg2pho_ind=bkg2.pho_index[0]
+        datapho_ind=data.pho_index[0]
+ #sr2,trailing photon
+    elif region==22:
+        bkg1pho_ind=bkg1.pho_index[1]
+        bkg2pho_ind=bkg2.pho_index[1]
+        datapho_ind=data.pho_index[1]
 
     hs=ROOT.THStack("hs","plotname")
 
@@ -201,9 +240,14 @@ def stackpho(plotname,branchname,region,data,dataname,bkg1,bkg1name,frac1,bkg2,b
 sw = ROOT.TStopwatch()
 sw.Start()
 print "start"
-data=ROOT.TFile("../selected/selected_dataSingleEle/selected_datasingleEle.root")
-mcttjets=ROOT.TFile("../selected/selected_mcttjets_321/selected_mcttjets_321.root")
-mcttwjets=ROOT.TFile("../selected/selected_mcttwjets_321/selected_mcttwjets_321.root")
+data=TFile.Open("../selected/selected_dataSingleEle_321/selected_dataSingleEle_321.root")
+#mcttjets=TFile.Open("../selected/selected_mcttjets_321/selected_mcttjets_321.root")
+#mcttwjets=TFile.Open("../selected/selected_mcttwjets_321/selected_mcttwjets_321.root")
+mcttg=TFile.Open("../selected/mcttgMar30/mcttg.root")
+mcttw=TFile.Open("../selected/mcttwMar30/mcttw.root")
+mctt=TFile.Open("../selected/mcttMar30/mctt.root")
+mcdyjets=TFile.Open("../selected/mcdyjetstollMar30/mcdyjetstoll.root")
+mcwjets=TFile.Open("../selected/mcwjetsMar30/mcwjets.root")
 
 # predata=data.Get("ggNtuplizer/EventTree_pre")
 # premcttjets=mcttjets.Get("ggNtuplizer/EventTree_pre")
@@ -218,17 +262,44 @@ mcttwjets=ROOT.TFile("../selected/selected_mcttwjets_321/selected_mcttwjets_321.
 # sr2mcttwjets=mcttwjets.Get("ggNtuplizer/EventTree_SR2")
 
 
-predata=data.Get("ggNtuplizer/EventTree_pre")
-premcttjets=mcttjets.Get("EventTree_pre")
-premcttwjets=mcttwjets.Get("EventTree_pre")
+predata=data.Get("EventTree_pre")
+sr1data=data.Get("EventTree_SR1")
+sr2data=data.Get("EventTree_SR2")
+cr1data=data.Get("EventTree_CR1")
+cr2data=data.Get("EventTree_CR2")
 
-sr1data=data.Get("ggNtuplizer/EventTree_SR1")
-sr1mcttjets=mcttjets.Get("EventTree_SR1")
-sr1mcttwjets=mcttwjets.Get("EventTree_SR1")
+premctt=mctt.Get("EventTree_pre")
+sr1mctt=mctt.Get("EventTree_SR1")
+sr2mctt=mctt.Get("EventTree_SR2")
+cr1mctt=mctt.Get("EventTree_CR1")
+cr2mctt=mctt.Get("EventTree_CR2")
 
-sr2data=data.Get("ggNtuplizer/EventTree_SR2")
-sr2mcttjets=mcttjets.Get("EventTree_SR2")
-sr2mcttwjets=mcttwjets.Get("EventTree_SR2")
+premcttw=mcttw.Get("EventTree_pre")
+sr1mcttw=mcttw.Get("EventTree_SR1")
+sr2mcttw=mcttw.Get("EventTree_SR2")
+cr1mcttw=mcttw.Get("EventTree_CR1")
+cr2mcttw=mcttw.Get("EventTree_CR2")
+
+
+premcttg=mcttg.Get("EventTree_pre")
+sr1mcttg=mcttg.Get("EventTree_SR1")
+sr2mcttg=mcttg.Get("EventTree_SR2")
+cr1mcttg=mcttg.Get("EventTree_CR1")
+cr2mcttg=mcttg.Get("EventTree_CR2")
+
+
+premcdyjets=mcdyjets.Get("EventTree_pre")
+sr1mcdyjets=mcdyjets.Get("EventTree_SR1")
+sr2mcdyjets=mcdyjets.Get("EventTree_SR2")
+cr1mcdyjets=mcdyjets.Get("EventTree_CR1")
+cr2mcdyjets=mcdyjets.Get("EventTree_CR2")
+
+
+premcwjets=mcwjets.Get("EventTree_pre")
+sr1mcwjets=mcwjets.Get("EventTree_SR1")
+sr2mcwjets=mcwjets.Get("EventTree_SR2")
+cr1mcwjets=mcwjets.Get("EventTree_CR1")
+cr2mcwjets=mcwjets.Get("EventTree_CR2")
 
 
 dd=datetime.datetime.now().strftime("%b%d")
@@ -237,7 +308,7 @@ os.chdir('plot_'+dd)
 
 #------------pre plot
 #--pfMET
-stack(predata,"SingleEle",premcttwjets,"bkg_ttwjets",frac_ttwjets,premcttjets,"bkg_ttjets",frac_ttjets,"Pre_pfMET","pfMET")
+stack("Pre_pfMET","pfMET",predata,"DataSingleEle",premctt,"bkg_tt",frac_tt,premcttg,"bkg_ttg",frac_ttg,premcttw,"bkg_ttw",frac_ttw,premcwjets,"bkg_wjets",frac_wjets,premcdyjets,"bkg_zjets",frac_dyjets)
 #--elePt and eleEta
 stackEle("pre_","elePt",predata,"SingleEle",premcttwjets,"bkg_ttwjets",frac_ttwjets,premcttjets,"bkg_ttjets",frac_ttjets)
 stackEle("pre_","eleEta",predata,"SingleEle",premcttwjets,"bkg_ttwjets",frac_ttwjets,premcttjets,"bkg_ttjets",frac_ttjets)
@@ -245,7 +316,8 @@ stackEle("pre_","eleEta",predata,"SingleEle",premcttwjets,"bkg_ttwjets",frac_ttw
 
 #------------sr1 plot
 #--pfMET
-stack(sr1data,"SingleEle",sr1mcttwjets,"bkg_ttwjets",frac_ttwjets,sr1mcttjets,"bkg_ttjets",frac_ttjets,"SR1pfMET","pfMET")
+#stack("SR1pfMET","pfMET",sr1data,"SingleEle",sr1mcttwjets,"bkg_ttwjets",frac_ttwjets,sr1mcttjets,"bkg_ttjets",frac_ttjets)
+stack("SR1_pfMET","pfMET",sr1data,"DataSingleEle",sr1mctt,"bkg_tt",frac_tt,sr1mcttg,"bkg_ttg",frac_ttg,sr1mcttw,"bkg_ttw",frac_ttw,sr1mcwjets,"bkg_wjets",frac_wjets,sr1mcdyjets,"bkg_zjets",frac_dyjets)
 #--elePt and eleEta
 stackEle("SR1_","elePt",sr1data,"SingleEle",sr1mcttwjets,"bkg_ttwjets",frac_ttwjets,sr1mcttjets,"bkg_ttjets",frac_ttjets)
 stackEle("SR1_","eleEta",sr1data,"SingleEle",sr1mcttwjets,"bkg_ttwjets",frac_ttwjets,sr1mcttjets,"bkg_ttjets",frac_ttjets)
@@ -257,7 +329,8 @@ stackpho("SR1_","phoEta",11,sr1data,"SingleEle",sr1mcttwjets,"bkg_ttwjets",frac_
 
 #------------sr2 plot
 #--pfMET
-stack(sr2data,"SingleEle",sr2mcttwjets,"bkg_ttwjets",frac_ttwjets,sr2mcttjets,"bkg_ttjets",frac_ttjets,"SR2pfMET","pfMET")
+#stack("SR2pfMET","pfMET",sr2data,"SingleEle",sr2mcttwjets,"bkg_ttwjets",frac_ttwjets,sr2mcttjets,"bkg_ttjets",frac_ttjets)
+stack("SR2_pfMET","pfMET",sr2data,"DataSingleEle",sr2mctt,"bkg_tt",frac_tt,sr2mcttg,"bkg_ttg",frac_ttg,sr2mcttw,"bkg_ttw",frac_ttw,sr2mcwjets,"bkg_wjets",frac_wjets,sr2mcdyjets,"bkg_zjets",frac_dyjets)
 #--elePt and eleEta
 stackEle("SR2_","elePt",predata,"SingleEle",premcttwjets,"bkg_ttwjets",frac_ttwjets,premcttjets,"bkg_ttjets",frac_ttjets)
 stackEle("SR2_","eleEta",predata,"SingleEle",premcttwjets,"bkg_ttwjets",frac_ttwjets,premcttjets,"bkg_ttjets",frac_ttjets)
