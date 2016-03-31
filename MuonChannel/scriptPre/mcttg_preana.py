277ba1e861f25230cfc8ec1e167e12e585ec16c2
fanxia <fanxia08@gmail.com>
2016-03-31 10:56:08 +0200

    muchannel pre

--- MuonChannel/scriptPre/mcttg_preana.py
+++ MuonChannel/scriptPre/mcttg_preana.py
@@ -1,8 +1,8 @@
 #!/bin/python
-# 3.17.2016 by Fan Xia
+# 3.31.2016 by Fan Xia
 # This template uses the ggntuples as input, make the preselection and get reduced root file which should be saved in dir:preselected. It can be very time consuming. This one is only for reducing event number purpose and get the first look of pre region. 
 # If tighter cuts apply to the preselect, only need to run script in dir:scriptANA again. 
-## This script pre select the dataSingeMu
+## This script pre select the mcttg
 
 import os
 import sys
@@ -18,23 +18,22 @@ from leptonANA.ElectronChannel.ana_jet import *
 sw = ROOT.TStopwatch()
 sw.Start()
 print "start"
-chain_in = ROOT.TChain("ggNtuplizer/EventTree")
-chain_in.Add("root://eoscms.cern.ch//store/group/phys_smp/ggNtuples/13TeV/data/V07_04_16_03/job_SingleMu_Run2015D_PR_v4_miniAOD.root")
-#chain_in.Add("root://eoscms.cern.ch//store/group/phys_smp/ggNtuples/13TeV/data/V07_04_16_03/data3/ggNtuples/V07_04_16_03/job_SingleEle_Run2015D_PR_v4_miniAOD.root")
-#chain_in.Add("root://eoscms.cern.ch//store/group/phys_smp/ggNtuples/13TeV/data/V07_04_16_03/data3/ggNtuples/V07_04_16_03/job_SingleEle_Run2015D_Oct05_miniAOD.root")
 
-chain_in.SetBranchStatus("tau*",0)
+chain_in = ROOT.TChain("ggNtuplizer/EventTree")
+chain_in.Add("root://eoscms.cern.ch//eos/cms/store/group/phys_smp/ggNtuples/13TeV/mc/V07_04_16_03/job_spring15_TTG_miniAOD.root")
+#chain_in.SetBranchStatus("tau*",0)
+chain_in.SetBranchStatus("AK8*",0)
 n_events = chain_in.GetEntries()
 print"Total events for processing: ",n_events
 
-dd=datetime.datetime.now().strftime("%b%d")
 log = open("logpre.txt","a")
-#os.mkdir("Output_SingleEle_v315",0755)
-os.system('mkdir -p ../preselected/Output_dataSingleMu3'+dd)
-os.chdir("../preselected/Output_dataSingleMu3"+dd)
 
 
-file_out = ROOT.TFile("reduced_dataSingleMu.root","recreate")
+dd=datetime.datetime.now().strftime("%b%d")
+os.system('mkdir -p ../preselected/Output_mcttg_'+dd)
+os.chdir("../preselected/Output_mcttg_"+dd)
+
+file_out = ROOT.TFile("reduced_mcttg.root","recreate")
 dir_out = file_out.mkdir("ggNtuplizer")
 dir_out.cd()
 tree_out = chain_in.CloneTree(0)
@@ -45,7 +44,7 @@ n_hlt=0
 n_singleMu=0
 n_pre=0
 
-#for i in range(10000):
+
 for i in range(n_events):
     chain_in.GetEntry(i)
     
@@ -97,10 +96,9 @@ for i in range(n_events):
     for j in range(chain_in.nJet):
         if good_LooseJet(chain_in.jetPt[j],chain_in.jetEta[j], chain_in.jetPFLooseId[j]):
             n_jet+=1
-
             if chain_in.jetpfCombinedInclusiveSecondaryVertexV2BJetTags[j]>0.89:
                 n_bjet+=1
-
+ #               bjetlist.append(j)
     if n_jet<3 or n_bjet<1:
         continue
     n_pre+=1
@@ -109,13 +107,12 @@ for i in range(n_events):
 #---------------------above for pre-selection---------------------
 
 
-
 file_out.Write()
 file_out.Close()
 
 print "----------------------"
 print "TotalEventNumber = ", n_events
-print "n_goodvtx pass =", n_goodvtx
+print "n_goodvtx pass = ",n_goodvtx
 print "n_hlt pass = ", n_hlt
 print "n_singleMu pass = ", n_singleMu
 print "n_pre selection = ",n_pre
@@ -123,17 +120,18 @@ print "----------------------"
 
 
 #### to write in logpre.txt
-
 log.write("############################################################\n")
+log.write("INPUT root://eoscms.cern.ch//eos/cms/store/group/phys_smp/ggNtuples/13TeV/mc/V07_04_16_03/job_spring15_TTG_miniAOD.root")
+
 log.write("%s"%datetime.datetime.now())
-log.write("\n-----dataSingleMuon----------\n")
-log.write("TotalEventNumber = %s"%n_events)
+log.write("\nTotalEventNumber = %s"%n_events)
 log.write("\nn_goodvtx pass = %s"%n_goodvtx)
 log.write ("\n n_hlt pass = %s"% n_hlt)
-log.write( "\nn_singleMu pass =%s "%n_singleMu)
+log.write( "\nn_singleEle pass =%s "%n_singleEle)
 log.write("\nn_pre selection = %s"%n_pre)
 log.write( "\n----------------------\n\n")
 log.close()
+
 
 sw.Stop()
 print "Real time: " + str(sw.RealTime() / 60.0) + " minutes"
---
+++ MuonChannel/scriptPre/run_mc_preana.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python mctt_preana.py
